0b15836a7ddfcf3a736904bf7ed43c637a584168
Your Name <you@example.com>
2022-04-07 20:17:47 +0600

    sob pray ses responsive er kaj baki ace

--- custom.js
+++ custom.js
@@ -1,15 +1,31 @@
 // get input balance
-function getInputBalance(){
+function getIncomeBalance(){
     const incomeField = document.getElementById('income-field')
-    let inputBalance = parseFloat(incomeField.value);
-    return inputBalance;
+    let incomeBalance = parseFloat(incomeField.value);
+    if(isNaN(incomeBalance) || incomeBalance < 0){
+        alert(`Negative or string type Balance aren't acceptable`);
+        return 0;
+     }else{
+         return incomeBalance;
+     }   
 }
 
-// get Expenses amout number
+// get Expenses amount number
 function getExpensesAmount(inputField){
+    const incomeBalance = getIncomeBalance();
     const field = document.getElementById(inputField);
     let fieldExpense = parseFloat(field.value);
-    return fieldExpense;
+    // check negative or string type value
+    if(isNaN(fieldExpense) || fieldExpense < 0){
+       alert(`Pls give positive number in ${inputField} \nCause you can't give negative or string type value in input filed`);
+       return 0;
+    }else if(fieldExpense > incomeBalance){
+        alert(`Opps..! ${inputField}  Amount is so high. \nPlease..! give less than ${incomeBalance} amount in ${inputField} \nOtherwise this filed aren't working`);
+        return 0;
+    }
+    else{
+        return fieldExpense;
+    }   
 }
 
 // get Balance
@@ -21,8 +37,8 @@ function getBalance(){
 
 document.getElementById('calculate').addEventListener("click",function(){
    /*  const incomeField = document.getElementById('income-field')
-    let inputBalance = parseFloat(incomeField.value); */
-    const inputBalance = getInputBalance();
+    let incomeBalance = parseFloat(incomeField.value); */
+    const incomeBalance = getIncomeBalance();
 
     //  expenses calculation
     /* const foodField = document.getElementById('food-field');
@@ -41,33 +57,37 @@ document.getElementById('calculate').addEventListener("click",function(){
     const totalExpenseField = document.getElementById('total-expense');
     const totalExpense = foodExpense + rentExpense + clothExpense;
     totalExpenseField.innerText = totalExpense;
+    if(totalExpense > incomeBalance){
+        alert(`Tomar total expenses income balance er theke besi hoey jacce.`)
+    }
     // balance field
     const balanceField = document.getElementById('balance')
-    const balance = inputBalance - totalExpense
+    const balance = incomeBalance - totalExpense
     balanceField.innerText = balance;
-})
+});
 
 // saving calculation
 document.getElementById('saving-btn').addEventListener('click', function(){
 /*     const incomeField = document.getElementById('income-field')
-    let inputBalance = parseFloat(incomeField.value); */
-    const inputBalance = getInputBalance()
+    let incomeBalance = parseFloat(incomeField.value); */
+    const incomeBalance = getIncomeBalance()
+    const previousBalaceAmount = getBalance()
 
     const saveAmountfield = document.getElementById('saving-field');
     const saveAmoutParcent = parseFloat(saveAmountfield.value)
 
     // percentage calculation
     const savingBalance = document.getElementById('save-amount');
-    const saveAmount =( inputBalance / 100) * saveAmoutParcent;
-    savingBalance.innerText = saveAmount;
+    const saveAmount =( incomeBalance / 100) * saveAmoutParcent;
 
-    // previous balance
-    /*  const previousBalace = document.getElementById('balance');
-    let previousBalaceAmount = parseFloat(previousBalace.innerText) */
-    const previousBalaceAmount = getBalance()
-
-    // remaining balance
-    const remainingBalanceArea = document.getElementById('remaining-balance');
-    const remainingBalance = previousBalaceAmount - saveAmount;
-    remainingBalanceArea.innerText = remainingBalance;
+    // handle error case if save amount greather than balance
+    if(saveAmount > previousBalaceAmount){
+       alert(`your saving amount is greater than your balance \nTomar balance er theke besi amount save korte parba na `);
+        savingBalance.innerText = 00;
+    }else{
+        savingBalance.innerText = saveAmount;
+        const remainingBalanceArea = document.getElementById('remaining-balance');
+        const remainingBalance = previousBalaceAmount - saveAmount;
+        remainingBalanceArea.innerText = remainingBalance;
+    }
 })
